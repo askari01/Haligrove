a2ed5af7a2f589deb1b3355ceadaa97802f8ba14
Phillip Carlino <flip902@home.local>
2018-07-03 21:08:52 -0300

    SignUpController

---
+++ Haligrove/Login/LoginController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginController.swift
+//  Haligrove
+//
+//  Created by Phillip Carlino on 2018-07-03.
+//  Copyright © 2018 Phillip Carlino. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class LoginController: UIViewController, UITextFieldDelegate {
+    
+}
